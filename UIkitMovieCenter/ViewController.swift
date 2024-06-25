7044afff3018e4d80bc18b09810adc5496dd2401
Rakibul Anam <rakibul.anam@brainstation-23.com>
2024-06-25 11:08:40 +0600

    Initial Commit

---
+++ UIkitMovieCenter/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  UIkitMovieCenter
+//
+//  Created by BS00834 on 25/6/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
